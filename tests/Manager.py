394d854dde26e49569cfb71c2e3d094a1f2cbb32
Peter Alabaster <peter.alabaster@opusvl.com>
2019-11-08 12:05:29 +0000

    [O2191] Use correct variable name for login in template

--- tests/Manager.py
+++ tests/Manager.py
@@ -10,7 +10,7 @@ class Manager(OdooRPCLocust):
     Attributes:
         host (str): ip or hostname of the host in which the tests are going to be run
         database: (str): name of the database
-        login (str): login of the user (odoo)
+        user (str): login of the user (odoo)
         password (str): password of that user
         port (int): listening port (odoo)
     """
@@ -19,7 +19,7 @@ class Manager(OdooRPCLocust):
     weight = 3
     host = environ["ODOO_HOST"]
     database = environ["ODOO_DATABASE"]
-    login = environ["ODOO_LOGIN"]
+    user = environ["ODOO_LOGIN"]
     password = environ["ODOO_PASSWORD"]
     port = int( environ["ODOO_PORT"] )
 
